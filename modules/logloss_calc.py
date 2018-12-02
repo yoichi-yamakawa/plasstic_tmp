326a5315a9592258745ec6eda6ba25f3828b8fc1
yoichi-yamakawa <yoichi.yamakawa@dena.com>
2018-12-03 01:23:25 +0900

    modules add

---
+++ modules/logloss_calc.py
@@ -0,0 +1,125 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import gc
+import os 
+import joblib
+from joblib import Parallel, delayed
+from time import time
+import time
+import pickle
+import sys
+import subprocess
+
+from sklearn.model_selection import StratifiedKFold
+import lightgbm as lgb
+from sklearn.model_selection import KFold
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import dask.dataframe as dd
+from dask.multiprocessing import get
+from tsfresh.feature_extraction import extract_features
+
+
+# In[6]:
+
+
+def multi_class_logloss(target, pred_y):
+    classes = [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95]
+    class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1}
+    
+    if len(np.unique(target)) > 14:
+        classes.append(99)
+        class_weight[99] = 2
+        
+    y_p = pred_y.reshape(target.shape[0], len(classes), order='F')
+    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1 - 1e-15)
+    y_p_log = np.log(y_p)    
+    
+    y_labels = pd.get_dummies(target)
+    y_log_loss = np.sum(y_p_log * y_labels, axis = 0)
+    
+    total_label_num = y_labels.sum(axis = 0).values.astype('float32')
+    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])    
+    y_w = y_log_loss * class_arr / total_label_num
+
+    loss = - np.sum(y_w) / np.sum(class_arr)
+    
+    return 'weighted multi logloss', loss, False
+
+def multi_weighted_logloss_valid(y_true, y_preds):
+    """
+    @author olivier https://www.kaggle.com/ogrellier
+    multi logloss for PLAsTiCC challenge
+    """
+    classes = [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95]
+    class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1}
+    if len(np.unique(y_true)) > 14:
+        classes.append(99)
+        class_weight[99] = 2
+    y_p = y_preds
+    # Trasform y_true in dummies
+    y_ohe = pd.get_dummies(y_true)
+    # Normalize rows and limit y_preds to 1e-15, 1-1e-15
+    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1-1e-15)
+    # Transform to log
+    y_p_log = np.log(y_p)
+    # Get the log for ones, .values is used to drop the index of DataFrames
+    # Exclude class 99 for now, since there is no class99 in the training set 
+    # we gave a special process for that class
+    y_log_ones = np.sum(y_ohe.values * y_p_log, axis=0)
+    # Get the number of positives for each class
+    nb_pos = y_ohe.sum(axis=0).values.astype(float)
+    # Weight average and divide by the number of positives
+    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+    y_w = y_log_ones * class_arr / nb_pos
+    
+    loss = - np.sum(y_w) / np.sum(class_arr)
+    return loss
+
+def lgb_multi_weighted_logloss(y_true, y_preds):
+    """
+    @author olivier https://www.kaggle.com/ogrellier
+    multi logloss for PLAsTiCC challenge
+    """
+    classes = [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95]
+    class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1}
+    if len(np.unique(y_true)) > 14:
+        classes.append(99)
+        class_weight[99] = 2
+    y_p = y_preds.reshape(y_true.shape[0], len(classes), order='F')
+    
+    # Trasform y_true in dummies
+    y_ohe = pd.get_dummies(y_true)
+    # Normalize rows and limit y_preds to 1e-15, 1-1e-15
+    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1-1e-15)
+    # Transform to log
+    y_p_log = np.log(y_p)
+    # Get the log for ones, .values is used to drop the index of DataFrames
+    # Exclude class 99 for now, since there is no class99 in the training set 
+    # we gave a special process for that class
+    y_log_ones = np.sum(y_ohe.values * y_p_log, axis=0)
+    # Get the number of positives for each class
+    nb_pos = y_ohe.sum(axis=0).values.astype(float)
+    # Weight average and divide by the number of positives
+    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+    y_w = y_log_ones * class_arr / nb_pos
+    
+    loss = - np.sum(y_w) / np.sum(class_arr)
+    return 'wloss', loss, False
+
+
+# In[7]:
+
+
+
+subprocess.run(['jupyter', 'nbconvert', '--to', 'python', 'logloss_calc.ipynb'])
+
